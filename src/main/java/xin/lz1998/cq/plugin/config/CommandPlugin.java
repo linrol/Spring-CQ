b2f7d7085cc48e247874f8acd272d5a809837a9f
linrol <lin1.luo@changhong.com>
2019-12-01 18:43:39 +0800

    优化

--- src/main/java/xin/lz1998/cq/plugin/config/CommandPlugin.java
+++ src/main/java/xin/lz1998/cq/plugin/config/CommandPlugin.java
@@ -26,7 +26,6 @@ public class CommandPlugin extends CQPlugin {
 		lcontrollerList.add(1071893649l);
 		config.put(CommandEnum.CONTROLLER_QQ_LIST.getCommand(), lcontrollerList);
 		List<Long> monitorGroupList = new ArrayList<Long>();
-		monitorGroupList.add(286624903l);
 		monitorGroupList.add(737215804l);
 		config.put(CommandEnum.MONITOR_GROUP_ID_LIST.getCommand(), monitorGroupList);
 		List<Long> forwardGroupList = new ArrayList<Long>();
