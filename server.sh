239a3eef667af295b1c94cbca4020df3cb3c04de
linrol <lin1.luo@changhong.com>
2019-12-03 21:58:59 +0800

    优化部署

--- server.sh
+++ server.sh
@@ -5,7 +5,7 @@ cmd=$1
 pid=`ps -ef|grep java|grep $app|awk '{print $2}'`
 
 startup(){
-  nohup java -jar $args $app > spring-coolq.log 2>&1 &
+  nohup /usr/local/java/jdk1.8.0_201/bin/java -jar $args $app > spring-coolq.log 2>&1 &
 }
 
 if [ ! $cmd ]; then
