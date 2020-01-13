0550ef58f5b35562ca19f6351f96d874584513ed
linrol <1071893649@qq.com>
2020-01-13 11:17:58 +0800

    邀请好友入群操作

--- src/main/java/xin/lz1998/cq/SpringCQApplication.java
+++ src/main/java/xin/lz1998/cq/SpringCQApplication.java
@@ -2,8 +2,10 @@ package xin.lz1998.cq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringCQApplication {
 
     public static void main(String[] args) {
