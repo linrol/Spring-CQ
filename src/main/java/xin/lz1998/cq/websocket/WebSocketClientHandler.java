8722bcf93068866029581400375c80f7b5b5a2c9
linrol <lin1.luo@changhong.com>
2020-01-13 08:10:40 +0800

    优化添加好友

--- src/main/java/xin/lz1998/cq/websocket/WebSocketClientHandler.java
+++ src/main/java/xin/lz1998/cq/websocket/WebSocketClientHandler.java
@@ -9,8 +9,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 
 import xin.lz1998.cq.Global;
+import xin.lz1998.cq.plugin.forward.HttpUtil;
 import xin.lz1998.cq.robot.qlight.Qlight;
 
 public class WebSocketClientHandler extends WebSocketClient {
@@ -29,7 +32,21 @@ public class WebSocketClientHandler extends WebSocketClient {
 		 robot.setSelfId(qlightSelfId);
 		 robot.setRobotWebSocketClient(this);
 		 CompletableFuture.runAsync(() -> {
-			 Global.qlightRobots.get(1706860030l).getGroupMemberList("479460338");
+			 // 753210700
+			 Global.qlightRobots.get(1706860030l).inviteIntoGroup("3021561689");
+			 /*try {
+				JSONObject jsonResult = HttpUtil.sendGet(String.format("http://www.alinkeji.com:8081/web_api/get_group_member_list?self_id=%s&group_id=%s", "779721310","753210700"));
+				JSONArray jsonArray = jsonResult.getJSONArray("data");
+				for(int i=0;i<200;i++) {
+					String friendQQ = jsonArray.getJSONObject(i).getString("user_id");
+					int random=(int)(Math.random()*40+20);
+					LOGGER.info("当前执行第{}条添加qq好友{}操作，并随机等待{}秒执行下一次添加",i,friendQQ,random);
+					Global.qlightRobots.get(1706860030l).addFriend(friendQQ);
+					Thread.sleep(random * 1000);
+				}
+			 } catch (Exception e) {
+				e.printStackTrace();
+			}*/
 		 });
 		 LOGGER.info("{} WebSocketClient open success....", qlightSelfId);
 	 }
