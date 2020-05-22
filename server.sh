8a6ba53787308a663e618127d84e15a2f4ae399b
linrol <linrol@77hub.com>
2020-05-22 09:40:19 +0800

    日志追加

--- server.sh
+++ server.sh
@@ -5,7 +5,7 @@ cmd=$1
 pid=`ps -ef|grep java|grep $app|awk '{print $2}'`
 
 startup(){
-  nohup /usr/local/java/jdk1.8.0_201/bin/java -jar $args $app > /root/web/app/coolq/logs/spring-coolq-`date +%Y%m%d%H%M`.log 2>&1 &
+  nohup /usr/local/java/jdk1.8.0_201/bin/java -jar $args $app >> /root/web/app/coolq/logs/spring-coolq-`date +%Y%m%d`.log 2>&1 &
 }
 
 if [ ! $cmd ]; then
